6a9f49cf22bf25a6b6e473a375c1255de6c425e8
Tsuyue <53194250+ShiratsuYudachi@users.noreply.github.com>
2023-05-29 03:25:15 +0800

    Create autofill.js

---
+++ autofill.js
@@ -0,0 +1,48 @@
+// CSV文件的URL
+let csvUrl = 'https://raw.githubusercontent.com/evnchn/LEGL1000-SB/master/M1.csv';
+
+// 使用fetch API获取CSV文件内容
+fetch(csvUrl)
+  .then(response => response.text())
+  .then(data => {
+    // 使用split函数按行分割数据，然后使用map函数将每行数据再按逗号分割
+    let allQuestionsText = data.split('\n').map(line => line.split(','));
+
+    // 打印第一个问题和答案以进行调试
+    console.log('第一个问题:', allQuestionsText[0][0]);
+    console.log('第一个问题的答案:', allQuestionsText[0][1]);
+
+    // 遍历所有问题
+    allQuestionsText.forEach(([questionText, answerText]) => {
+      // 尝试在页面上找到匹配的问题
+      let questionElement = Array.from(document.querySelectorAll('.question_text')).find(element => {
+        // 删除所有HTML标签和额外的空格
+        let cleanedText = element.innerHTML.replace(/<[^>]*>/g, '').replace(/\s+/g, ' ').trim();
+        return cleanedText === questionText;
+      });
+
+      if (questionElement) {
+        // 找到问题后，获取其父元素（问题容器）
+        let questionContainer = questionElement.parentElement;
+
+        // 在问题容器内找到所有的答案元素
+        let answerElements = questionContainer.querySelectorAll('.answer_label');
+
+        // 打印当前问题框中的问题和所有答案
+        console.log('当前问题:', questionElement.textContent.trim());
+        console.log('所有答案:', Array.from(answerElements).map(element => element.textContent.trim()));
+
+        // 在答案元素中找到匹配的答案
+        let answerElement = Array.from(answerElements).find(element => element.textContent.trim() === answerText);
+
+        if (answerElement) {
+          // 找到答案后，获取其父元素（答案容器）
+          let answerContainer = answerElement.parentElement;
+
+          // 在答案容器内找到输入元素（单选按钮）并选中它
+          let inputElement = answerContainer.querySelector('.question_input');
+          inputElement.checked = true;
+        }
+      }
+    });
+  });
